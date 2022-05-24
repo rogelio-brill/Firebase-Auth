a26b9d17a9f39abc7c6a370535ba0d142b85280a
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-05-23 17:29:23 -0700

    Setup html template and signup feature

---
+++ scripts/auth.js
@@ -0,0 +1,18 @@
+// Signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // Sign up the user
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(cred => {
+            console.log(cred.user);
+            const modal = document.querySelector('#modal-signup');
+            M.Modal.getInstance(modal).close();
+            signupForm.reset();
+        });
+});
