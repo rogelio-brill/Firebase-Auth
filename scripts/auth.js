03e518abb88e92ac65e39c99667299a39135e266
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-05-24 17:12:18 -0700

    loggin and logout features work correctly

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,17 @@
+// Listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if(user) {
+        // Get data
+        db.collection('guides').get().then(snapshot => {
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        });
+    }else {
+        setupUI();
+        setupGuides([]);
+    }
+});
+
 // Signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -10,9 +24,32 @@ signupForm.addEventListener('submit', (e) => {
     // Sign up the user
     auth.createUserWithEmailAndPassword(email, password)
         .then(cred => {
-            console.log(cred.user);
             const modal = document.querySelector('#modal-signup');
             M.Modal.getInstance(modal).close();
             signupForm.reset();
+        });
+});
+
+// Logout 
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+// Login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // Get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then(cred => {
+            const modal = document.querySelector('#modal-login');
+            M.Modal.getInstance(modal).close();
+            loginForm.reset();
         });
 });
